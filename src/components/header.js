350140905271e34310ded65b6edefb4c60e52a5d
php1301 <19520854@gm.uit.edu.vn>
2021-01-25 22:12:01 +0700

    callAPI

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -21,8 +21,10 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ButtonAppBar({ children }) {
   const {state, dispatch} = useContext(ThemeContext)
+
+  const [demo, setDemo] = useState(true)
   const classes = useStyles();
-  const changeColor = () =>{
+  const changeColor = () => {
     dispatch({
       type: 'CHANGE',
       payload: {
@@ -30,6 +32,60 @@ export default function ButtonAppBar({ children }) {
       }
     })
   }
+  // componentDidUpdate
+  // Tại vì state đã đổi
+  // chạy khi mỗi lần component đó bị tác động đến
+  // DOM, ref (UI bị thay đổi)
+
+  // useEffect(()=>{
+  //   console.log("a")
+  // })
+
+  // componentDidMount
+  // Dependencies array - nhận các variables
+  // Component scope
+  // effect chạy hay ko
+  // Tùy thuộc vào variables
+  // Có thay đổi hay ko
+  // dependency rỗng, ko dựa vào ai, chỉ chạy effect 1 lần
+
+  // useEffect(()=>{
+  //   console.log("a")
+  //   // setDemo(false)
+  // },[])
+
+
+  // componentWillMount
+  // update có đk, có dependency
+  // demo thay đổi thì chạy lại hàm dưới
+  
+  // useEffect(()=>{
+  //   console.log("a da bi goi lai")
+  // },[demo])
+
+
+  // componentWillUnmount
+  // Đảm bảo là các state cũ
+  // Đã được hủy
+  // Khi mà component được hủy
+  // Thường xài cho các hành động
+  // Cần update state liên tục
+  // Như chatapp,...
+
+
+  // useEffect(()=>{
+  //   console.log("a")
+  //   // Cleanup
+  //   return () =>{
+  //     // các vd
+  //     // Chat.unsubscribe
+  //     // loader false
+  //     // xóa bỏ state
+  //   }
+  // })
+
+
+
   return (
       <>
     <div className={classes.root}>
--- src/components/userProfile/userProfile.component.js
+++ src/components/userProfile/userProfile.component.js
@@ -1,8 +1,9 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Wrapper, FieldWrappers } from './userProfile.styles'
 import Heading from '../heading/heading.component'
 import TextField from '../textField'
 import UserDetailsContext from '../../context/userDetails.context'
+import Loader from '../loader'
 
 const UserProfile = () => {
   const {
@@ -13,15 +14,49 @@ const UserProfile = () => {
     secretAnswer,
     setUserDetails
   } = useContext(UserDetailsContext)
+  // Tạo state chứa data
+  const [data, setData] = useState([])
 
+//  data kéo về
+//  sẽ có delay
+//  thì ta sử dụng cái gì đó lắp vào cái delay
+//  Như loader
+  const [loading, isLoading] = useState(true)
+  useEffect(()=>{
+// gọi api để lấy data
+// có các hàm gọi api như fetch, axios, ajax, isomorphic-unfetch, swr,...
+  let mount = true
+    const fetchData  = async () => {
+      const dataFetched = await fetch("https://600ecd3e3bb1d100179e02a0.mockapi.io/users",{
+        method:'GET', // ko cần cũng dc, nó mặc định là GET
+      })
+      // Biến data thành json
+      const finalData = await dataFetched.json()
+      // Lưu vào state
+      setData(finalData)
+      
+    }
+    if(mount){
+    // Gọi hàm
+    fetchData()
+    }
+    return () => {mount = false}
+  }, [])
+  // Kiểm tra xem in data ok chưa
+  // console.log(data[0].name)
+  // lưu ý data đang là array
+  // Phải check null
+  // Phải check null
+  // Phải check null
   return (
-    <Wrapper>
+    <>
+    {data && data.length > 0 ? (<Wrapper>
       <Heading>My details</Heading>
 
       <FieldWrappers>
         <TextField
           label='name'
-          value={name}
+          value={data[0].name}
           onChange={e => {
             setUserDetails({ name: e.target.value })
           }}
@@ -48,14 +83,18 @@ const UserProfile = () => {
           }}
         />
         <TextField
-          label='secret answer'
-          value={secretAnswer}
+          label='age'
+          value={data[0].age}
           onChange={e => {
             setUserDetails({ secretAnswer: e.target.value })
           }}
         />
       </FieldWrappers>
+      <img src={data[0].avatar} width="400px" height="400px"/>
     </Wrapper>
+    ) : (<Loader />)
+     }
+    </>
   )
 }
 
